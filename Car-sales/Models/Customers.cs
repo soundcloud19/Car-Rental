afbec832d07bb4434b27dda352abb3a45dc4fe42
soundcloud19 <57188292+soundcloud19@users.noreply.github.com>
2019-11-29 18:52:22 +0100

    Dodaj pliki projektów.

---
+++ Car-sales/Models/Customers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car_sales.Models
+{
+    public class Customers
+    {
+        public long Customers_ID { get; set; }
+        public string Customer_Details { get; set; }
+        
+    }
+}
