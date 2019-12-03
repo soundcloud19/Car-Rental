7d09ae8505e8a09dc060cb65741731a04b2cb13d
soundcloud19 <57188292+soundcloud19@users.noreply.github.com>
2019-12-03 03:00:42 +0100

    add database context

---
+++ Car-sales/Models/CarSalesContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_sales.Models
+{
+    public class CarSalesContext : DbContext
+    {
+        public CarSalesContext(DbContextOptions<CarSalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Deliveries> Deliveries { get; set; }
+        public DbSet<RefModelTypes> RefModelTypes { get; set; }
+        public DbSet<Vehicles> Vehicles { get; set; }
+
+    }
+}
